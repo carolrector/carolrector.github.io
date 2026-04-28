f9e99f2e2de0462fae7772d5bd96b5d4bbfa2e80
carolrector <carolrector26@vt.edu>
2026-04-28 16:05:50 -0400

    spotify

---
+++ spotify-json/spotify-charts.js
@@ -0,0 +1,46 @@
+const spotifyChart =
+// paste code from Canvas below //
+{
+  "countryFilters": [],
+  "displayChart": {
+    "date": "2026-04-23",
+    "chartMetadata": {
+      "uri": "spotify:chart:7zE64clO2DB2y74QymNWRn",
+      "alias": "REGIONAL_GLOBAL_WEEKLY",
+      "entityType": "TRACK",
+      "readableTitle": "Weekly Top Songs: Global",
+      "backgroundColor": "#773795",
+      "textColor": "#FFFFFF",
+      "dimensions": {
+        "latestDate": "2026-04-23",
+        "earliestDate": "2016-12-29",
+        "country": "GLOBAL",
+        "chartType": "TOP_TRACK",
+        "recurrence": "WEEKLY",
+        "genre": "",
+        "city": ""
+      }
+    },
+    "description": "Your weekly update of the most played tracks right now."
+  },
+  "entries": [
+    {
+      "chartEntryData": {
+        "currentRank": 1,
+        "previousRank": 3,
+        "peakRank": 0,
+        "appearancesOnChart": 0,
+        "consecutiveAppearancesOnChart": 0,
+        "entryStatus": "MOVED_UP",
+        "peakDate": "",
+        "entryRank": 0,
+        "entryDate": ""
+      },
+      "missingRequiredFields": false,
+      "trackMetadata": {
+        "trackName": "Beauty And A Beat",
+        "trackUri": "spotify:track:6QFCMUUq1T2Vf5sFUXcuQ7",
+        "displayImageUri": "https://i.scdn.co/image/ab67616d00001e02f1d02a6cec967f8b6b78f76e",
+        "artists": [
+          {
+            "name": "Justin Bie
